d99d412c05fb135168c5902c639e8d0c800c16fa
normchou <normchou@users.noreply.github.com>
2015-03-31 20:48:10 -0400

    flash cards from database, buttons to filter by category

--- public/app.js
+++ public/app.js
@@ -1,5 +1,25 @@
 var app = angular.module('flashCards', []);
 
+app.factory('FlashCardsFactory', function ($http) {
+    return { 
+        getFlashCards: function (category) {
+
+            var queryParams = {};
+
+            if (category) {
+                queryParams.category = category;
+            }
+
+            return $http.get('/cards', {
+                params: queryParams
+            }).then(function (response) {
+                return response.data;
+            });
+
+        }
+     };
+});
+
 app.value('whateverName', [
     {
         question: 'C. What is Angular?',
--- public/main.controller.js
+++ public/main.controller.js
@@ -1,5 +1,22 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function ($scope, whateverName, FlashCardsFactory) {
+	FlashCardsFactory.getFlashCards().then(function (data) {
+		$scope.flashCards = data;
+	});
+
+	$scope.getCategoryCards = function (category) {
+		FlashCardsFactory.getFlashCards(category).then(function (data) {
+			$scope.flashCards = data;
+		});
+	}
+
+	// $scope.flashCards = FlashCardsFactory.getFlashCards();
+
+	$scope.categories = [
+	    'MongoDB',
+	    'Express',
+	    'Angular',
+	    'Node'
+	];
 
 	$scope.answerQuestion = function (answer, flashCard) {
 		if (!flashCard.answered) {
